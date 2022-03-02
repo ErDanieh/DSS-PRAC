203a03a5c08062b34e18116ddc59584e5b9e12f7
Daniel <71588528+ErDanieh@users.noreply.github.com>
2022-03-02 15:38:07 +0100

    Update EjercicioTest.php

--- miproyecto/tests/Unit/EjercicioTest.php
+++ miproyecto/tests/Unit/EjercicioTest.php
@@ -19,4 +19,24 @@ class EjercicioTest extends TestCase
 
         $this->assertInstanceOf(Collection::class, $ejercicio->grupoMusculares);
     }
+
+    public function test_Ejercicios_select()
+    {
+        {
+            $arrayEsperado = [
+              "Press de pecho plano con mancuerna."
+            , "Press de pecho con mancuerna inclinado"
+            , "Apertura de mancuernas"
+            , "Cruces en polea alta"
+            , "Cruces en polea baja"
+            , "Extensión de cuadriceps "
+            , "Curl Femoral"
+            , "Elevación de gemelos en máquina"
+            , "Abductor en máquina"
+            , "Press Militar con mancuernas sentado"
+            , "Elevaciones laterales"];
+            $grupoMuscular = Ejercicio::pluck('name')->toArray();
+            $this->assertEquals($arrayEsperado, $grupoMuscular);
+        }
+    }
 }
