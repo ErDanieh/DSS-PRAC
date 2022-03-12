2d0144f186f7b8c13697904de636712ea2b0dbf2
Daniel <71588528+ErDanieh@users.noreply.github.com>
2022-03-12 18:32:19 +0100

    Update EjerciciosTableSeeder.php

--- database/seeders/EjerciciosTableSeeder.php
+++ database/seeders/EjerciciosTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ejercicio;
 use Illuminate\Database\Seeder;
 use DB;
 
@@ -16,69 +17,94 @@ class EjerciciosTableSeeder extends Seeder
     {
         DB::table('ejercicios')->delete();
 
+        $ejercicio = new Ejercicio([
+            'name' => 'Press de pecho plano con mancuerna.',
+            'descripcion' => 'Nos tumbaremos en un banco colocado de manera plana y empujaremos las mancuernas hacia arriba a la altura de nuestro pecho.',
+            'url_img' => 'https://wger.de/media/exercise-images/97/Dumbbell-bench-press-2.png'
+        ]);
+
+        $ejercicio->save();
+
+        $grupoMuscularPecho = DB::table('grupo_musculars')->where('name', 'Pectorales')->first();
+
+        $ejercicio->grupoMusculares()->attach($grupoMuscularPecho->id);
+
 
         DB::table('ejercicios')->insert(
-            ['name' => 'Press de pecho plano con mancuerna.',
-             'descripcion' => 'Nos tumbaremos en un banco colocado de manera plana y empujaremos las mancuernas hacia arriba a la altura de nuestro pecho.',
-             'url_img' => 'https://wger.de/media/exercise-images/97/Dumbbell-bench-press-2.png']
-        );
-
-        DB::table('ejercicios')->insert(
-            ['name' => 'Press de pecho con mancuerna inclinado',
-             'descripcion' => 'Nos tumbaremos en un banco colocado de manera inclinada y empujaremos las mancuernas hacia arriba a la altura de nuestro pecho.',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
-        );
-
-        DB::table('ejercicios')->insert(
-            ['name' => 'Apertura de mancuernas',
-             'descripcion' => 'Nos tumbaremos en un banco colocado de manera plana, subiremos los brazos y los abriremos y cerraremos con las mancuernas en las manos.',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
-        );
-        DB::table('ejercicios')->insert(
-            ['name' => 'Cruces en polea alta',
-             'descripcion' => 'En las poleas mixtas colocaremos los agarres arriba y cruzaremos los brazos formando una X',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
-        );
-        DB::table('ejercicios')->insert(
-            ['name' => 'Cruces en polea baja',
-             'descripcion' => 'En las poleas mixtas colocaremos los agarres baja y cruzaremos los brazos formando una X',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
-        );
-        DB::table('ejercicios')->insert(
-            ['name' => 'Extensión de cuadriceps ',
-             'descripcion' => 'Eleveramos las piernas en la máquina de extensión de cuadriceps',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
+            [
+                'name' => 'Press de pecho con mancuerna inclinado',
+                'descripcion' => 'Nos tumbaremos en un banco colocado de manera inclinada y empujaremos las mancuernas hacia arriba a la altura de nuestro pecho.',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
         );
 
         DB::table('ejercicios')->insert(
-            ['name' => 'Curl Femoral',
-             'descripcion' => 'Eleveramos las piernas en la máquina de curl de femoral',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
+            [
+                'name' => 'Apertura de mancuernas',
+                'descripcion' => 'Nos tumbaremos en un banco colocado de manera plana, subiremos los brazos y los abriremos y cerraremos con las mancuernas en las manos.',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
+        );
+        DB::table('ejercicios')->insert(
+            [
+                'name' => 'Cruces en polea alta',
+                'descripcion' => 'En las poleas mixtas colocaremos los agarres arriba y cruzaremos los brazos formando una X',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
+        );
+        DB::table('ejercicios')->insert(
+            [
+                'name' => 'Cruces en polea baja',
+                'descripcion' => 'En las poleas mixtas colocaremos los agarres baja y cruzaremos los brazos formando una X',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
+        );
+        DB::table('ejercicios')->insert(
+            [
+                'name' => 'Extensión de cuadriceps ',
+                'descripcion' => 'Eleveramos las piernas en la máquina de extensión de cuadriceps',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
         );
 
         DB::table('ejercicios')->insert(
-            ['name' => 'Elevación de gemelos en máquina',
-             'descripcion' => 'Nos pondremos de puntillas con el peso en los hombros en la máquina de elevación de gemelos',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
+            [
+                'name' => 'Curl Femoral',
+                'descripcion' => 'Eleveramos las piernas en la máquina de curl de femoral',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
         );
 
         DB::table('ejercicios')->insert(
-            ['name' => 'Abductor en máquina',
-             'descripcion' => 'Cerraremos las piernas en abductor en máquina de manera controlada.',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
+            [
+                'name' => 'Elevación de gemelos en máquina',
+                'descripcion' => 'Nos pondremos de puntillas con el peso en los hombros en la máquina de elevación de gemelos',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
         );
 
         DB::table('ejercicios')->insert(
-            ['name' => 'Press Militar con mancuernas sentado',
-             'descripcion' => 'En un banco sentados con el respaldo a 90 grados elevaremos de manera vertical las mancuernas por encima de la cabeza.',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
+            [
+                'name' => 'Abductor en máquina',
+                'descripcion' => 'Cerraremos las piernas en abductor en máquina de manera controlada.',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
         );
 
         DB::table('ejercicios')->insert(
-            ['name' => 'Elevaciones laterales',
-             'descripcion' => 'Eleveramos las mancuernas a los lados de nuestro cuerpo de manera controlada.',
-             'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg']
+            [
+                'name' => 'Press Militar con mancuernas sentado',
+                'descripcion' => 'En un banco sentados con el respaldo a 90 grados elevaremos de manera vertical las mancuernas por encima de la cabeza.',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
         );
 
+        DB::table('ejercicios')->insert(
+            [
+                'name' => 'Elevaciones laterales',
+                'descripcion' => 'Eleveramos las mancuernas a los lados de nuestro cuerpo de manera controlada.',
+                'url_img' => 'https://depowerlifting.site/wp-content/uploads/2021/06/ejercicios-para-hombros-1024x683-1.jpeg'
+            ]
+        );
     }
 }
