80a36420bdf5f76de9c04ab724e5bb3978b57ca9
Ander Dorado <71599694+ander-db@users.noreply.github.com>
2022-04-03 12:19:24 +0200

    Selectable in progress

---
+++ public/js/utils.js
@@ -0,0 +1,7 @@
+/**
+ * Devuelve la url de la que
+ * @param {*} id  
+ */
+function redirectToId(id){
+    window.location.href = `${window.location.href}/${id}`;
+}
--- resources/views/musculos/musculos.blade.php
+++ resources/views/musculos/musculos.blade.php
@@ -6,27 +6,29 @@
 
     <h1>Músculos</h1>
 
+    @include('common.alert')
+
     <div class="table-responsive">
         <table class="table">
             <thead>
-                <tr>
+                <tr style="font-size: 1.2rem;">
                     <th scope="col">Nombre</th>
                     <th scope="col">Grupo Muscular</th>
-                    <th scope="col">Editar</th>
-                    <th scope="col">Eliminar</th>
+                    <th scope="col" style="width: min-content;">Editar</th>
+                    <th scope="col" style="width: min-content;">Eliminar</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($musculos as $musculo)
                 <tr>
-                    <td> {{$musculo->name}} </td>
-                    <td>
+                    <td style="font-size: 1.2rem; font-weight: 400; margin-right: 5rem;"> {{$musculo->name}} </td>
+                    <td style="font-size: 1.2rem; font-weight: 100; margin-right: 5rem;">
                         {{$musculo->grupoMuscular->name}}
                     </td>
-                    <td>
-                        <button class="btn btn-success" type="submit">Editar</button>
+                    <td style="width: 50px;">
+                        <button class="btn btn-success" type="submit" style="font-size: 1rem;">Editar</button>
                     </td>
-                    <td>
+                    <td style="width: 50px; margin-right: 50px;">
                         <form action="{{url('/admin/musculos', $musculo->id)}}" class="mr-4" method="POST">
                             @csrf
                             {{ method_field('DELETE') }}
@@ -39,7 +41,16 @@
         </table>
     </div>
 
-    @include('common.alert')
+
+    <select class="form-select" aria-label="Default select example">
+        <option selected>Grupo Muscular</option>
+        <option value="1">Abdominales</option>
+        <option value="2">Espalda</option>
+        <option value="3">Brazo</option>
+        <option value="3">Brazo</option>
+    </select>
+
+
     <div class="m-3 justify-content-center text-dark">
         <form action="{{ url('/admin/musculos') }}" method="POST">
             @csrf
@@ -57,6 +68,9 @@
             </div>
         </form>
     </div>
+
+
+
 
 </div>
 @endsection
