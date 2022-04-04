72b5ec9275cdfa3bfe1f956b33933c313ce0681a
Ander Dorado <71599694+ander-db@users.noreply.github.com>
2022-04-04 13:18:16 +0200

    Blade musculo detalle

--- resources/views/musculos/musculoDetalle.blade.php
+++ resources/views/musculos/musculoDetalle.blade.php
@@ -1,22 +1,33 @@
-<h1>Esta es la información del musculo {{$musculo->name}}</h1>
-<h3>Pertenece al grupo muscular de: {{$musculo->grupoMuscular->name}}</h3>
+@extends('layouts.admin')
 
 
 
-@include('common.alert')
-<div class="m-3 justify-content-center text-dark">
-    <form action="{{ url('/admin/musculos',$musculo->id)}}" method="POST">
-        @csrf
-        {{ method_field('PUT') }}
-        <div class="mb-3">
-            <label for="name">Nombre del Musculo</label>
-            <input type="text" name="name" id="name">
+@section('content')
+<script src="../js/users.js"> </script>
+<div>
+    <h1>Editar musculo.</h1>
+    <h3>Nombre: {{$musculo->name}}</h3>
+    <h3>Grupo muscular: {{$musculo->grupoMuscular->name}}</h3>
 
-            <label for="grupo">Grupo Muscular al que pertenece</label>
-            <textarea type="text" name="grupo" id="grupo"></textarea>
-        </div>
-        <div class="text-right">
-            <button type="submit">Enviar</button>
-        </div>
-    </form>
+    @include('common.alert')
+    <div class="" style="margin-top: 20px;">
+        <form action="{{ url('/admin/musculos',$musculo->id)}}" method="POST">
+            @csrf
+            {{ method_field('PUT') }}
+            <div class="form-group">
+                <label for="name">Nombre del Musculo</label>
+                <input class="form-control" placeholder="Nombre del músculo..." required type="text" name="name" id="name">
+            </div>
+            <div class="form-group">
+                <label for="grupo">Grupo Muscular al que pertenece</label>
+                <select name="grupo" class="form-select">
+                    {{ App\Http\Controllers\MusculosController::seleccionableGruposMusculares();}}
+                </select>
+            </div>
+            <div class="text-right">
+                <button class="btn btn-primary" type="submit" style="margin-top: 20px;">Editar Musculo</button>
+            </div>
+        </form>
+    </div>
 </div>
+@endsection
