b48423f16d1049371657e72fcad667fc13c70360
Ander Dorado <71599694+ander-db@users.noreply.github.com>
2022-04-05 21:54:40 +0200

    Dashboard admin ammpliado

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -5,11 +5,47 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\User;
+use App\Models\Entrenamiento;
+use App\Models\Ejercicio;
+use App\Models\Musculo;
+use App\Models\GrupoMuscular;
+
 class AdministracionController extends Controller
 {
     function getAdministracion()
     {
-        return view("layouts.admin");
+        return view("admin.admin");
+    }
+
+    static function numUsers()
+    {
+        return User::all()->count();
+    }
+
+    static function numEntrenadores()
+    {
+        return User::where('is_trainer', '=', 1)->count();
+    }
+
+    static function numEntrenamientos()
+    {
+        return Entrenamiento::all()->count();
+    }
+
+    static function numEjercicios()
+    {
+        return Ejercicio::all()->count();
+    }
+
+    static function numMusculos()
+    {
+        return Musculo::all()->count();
+    }
+
+    static function grupoMuscular()
+    {
+        return GrupoMuscular::all()->count();
     }
 
 }
