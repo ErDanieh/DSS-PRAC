95a24dd4fa41bc21be66f55a8f55218cae6da0aa
Ander Dorado <71599694+ander-db@users.noreply.github.com>
2022-04-09 18:16:45 +0200

    JS actualizado

--- public/js/utils.js
+++ public/js/utils.js
@@ -10,10 +10,10 @@ function redirectSearch() {
     window.location.href = `${window.location.origin + window.location.pathname}?search=${document.getElementById('search').value}`;
 }
 
-function redirectOrder() {
+function redirectUrlParams($nameParam) {
     let queryString = window.location.search;  // get url parameters
     let params = new URLSearchParams(queryString);  // create url search params object
-    params.delete('ordered');  // delete city parameter if it exists, in case you change the dropdown more then once
-    params.append('ordered', document.getElementById("order").value); // add selected city
+    params.delete($nameParam);  // delete city parameter if it exists, in case you change the dropdown more then once
+    params.append($nameParam, document.getElementById($nameParam).value); // add selected city
     document.location.href = "?" + params.toString(); // refresh the page with new url
 }
