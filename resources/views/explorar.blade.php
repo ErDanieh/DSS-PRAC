5cbe259e53361e5ae9d7a70eb05f04962b332f19
Ander Dorado <71599694+ander-db@users.noreply.github.com>
2022-05-12 11:49:36 +0200

    Explorador actualizado

--- resources/views/explorar.blade.php
+++ resources/views/explorar.blade.php
@@ -4,8 +4,23 @@
     <div style="width: 900px; margin: auto;">
         <h1>Pagina de explorar</h1>
 
-        @foreach($entrenamientos as $entrenamiento)
-            <div>{{$entrenamiento->name}}</div>
-        @endforeach
+        <div class="container">
+            @foreach($entrenamientos->chunk(2) as $subentrenamiento)
+            <div class="row">
+                @foreach($subentrenamiento as $entrenamiento)
+                    <div class="col card" style="margin: 10px; max-width: 46%;">
+                        <img class="card-img-top" src="{{$entrenamiento->url_img}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$entrenamiento->name}}</h5>
+                            <p class="card-text">{{Str::limit($entrenamiento->descripcion, 50)}}</p>
+
+                            <a href="#" class="btn btn-primary">Más información</a>
+                        </div>
+                    
+                    </div>
+                @endforeach
+            </div>
+            @endforeach
+        </div>
     </div>
 @endsection
