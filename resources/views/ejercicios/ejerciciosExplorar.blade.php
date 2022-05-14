3ed90dde0914e087d2ebd5bad68595136efa2b5d
Ander Dorado <71599694+ander-db@users.noreply.github.com>
2022-05-14 17:08:32 +0200

    Modifivaviones en los estilos

--- resources/views/ejercicios/ejerciciosExplorar.blade.php
+++ resources/views/ejercicios/ejerciciosExplorar.blade.php
@@ -4,12 +4,12 @@
 @section('content')
 <script src="../js/utils.js"> </script>
 
-<div class="container">
-    <h1>Página de explorar ejercicios</h1>
-    @foreach($ejercicios->chunk(2) as $ejerciciosChunk)
-    <div class="row">
-        @foreach($ejerciciosChunk as $ejercicio)
-        <div class="col card" style="margin: 10px; max-width: 46%;">
+<div class="container min-vh-100 px-1 mb-5">
+
+    <h1 class="mt-4">Pagina de ejercicios</h1>
+    <div class="row justify-content-around">
+        @foreach($ejercicios as $ejercicio)
+        <div class="col-md-5 card mt-4 mx-4" style="padding: 0;">
             <img class="card-img-top" style="height: 15vw; object-fit: cover; width: auto;" src="{{$ejercicio->url_img}}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">{{$ejercicio->name}}</h5>
@@ -20,6 +20,5 @@
         </div>
         @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
