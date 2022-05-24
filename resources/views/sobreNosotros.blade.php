78424f7e5df2f979c2028b0392c043b0700bc2f8
jsdm1-ua <78562779+jsdm1-ua@users.noreply.github.com>
2022-05-24 16:48:23 +0200

    Página Sobre Nostros

--- resources/views/sobreNosotros.blade.php
+++ resources/views/sobreNosotros.blade.php
@@ -1,4 +1,36 @@
-@extends('layouts.principal')
 
+@extends('layouts.principal')
 @section('content')
-@endsection('content')
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+</head> 
+
+
+<div class="container my-5" id="abouttextstyle">
+   <div class="row">
+     <div class="col-md-6 p-4 p-sm-5 order-2 order-sm-1">
+       <small class="text-uppercase" style="color: #9B5DE5;"></small>
+       <h1 class="h2 mb-4" style="font-weight: 600;">Donde <span style="color: #9B5DE5;">Encontrarnos</span></h1>
+       <p class="text-secondary" style="line-height: 2;">Nuestras oficinas están situadas en la ciudad de Alicante (Comunidad Valenciana), concretamente en la universidad de Alicante en el edificio de la
+       politécnica superior.
+</p>
+       
+     </div>
+     <div class="col-md-6 p-0 text-center order-1 order-sm-2">
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3127.432639578668!2d-0.5165047842466574!3d38.38524457965288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6236bb72bf619b%3A0x506e11c403138428!2sUniversidad%20de%20Alicante!5e0!3m2!1ses!2ses!4v1653402202897!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+     </div>
+
+    <div class="col-md-6 p-0 text-center">
+       <img src="https://img.lalr.co/cms/2018/03/28195310/Running-Pons-Trainingok.jpg?size=xl&ratio=r40_21" class="w-100" alt="">
+    </div>
+    <div class="col-md-6 p-4 p-sm-5">
+      <small class="text-uppercase" style="color: #9B5DE5;"></small>
+      <h1 class="h2 mb-4" style="font-weight: 600;">About<span style="color: #9B5DE5;"> us</span></h1>
+      <p class="text-secondary" style="line-height: 2;">Debido a la pandemia que afectó a todo el planeta un grupo de 4 estudiantes de ingeniería informática decidimos juntarnos
+    y crear una página para poder seguir realizando deporte independientemente del confinamiento. Como es sabido por todos el deporte es una actividad fundamental para nuestra 
+salud y nuestro desarrollo personal es por ello que hemos decidio realizar esta página</p>
+      
+    </div>
+   </div>
+ </div>
+@endsection
