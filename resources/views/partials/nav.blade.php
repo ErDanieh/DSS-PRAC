e85d6cc11547075e7f33d72a77104574057d0afb
jsdm1-ua <78562779+jsdm1-ua@users.noreply.github.com>
2022-05-28 11:33:30 +0200

    Cambio de nombre

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -10,7 +10,7 @@
 
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <li><a href="/" class="nav-link px-2 link-secondary">Inicio</a></li>
-        <li><a href="/entrenamientos" class="nav-link px-2 link-dark">Explorar</a></li>
+        <li><a href="/entrenamientos" class="nav-link px-2 link-dark"> Entrenamientos</a></li>
         <li><a href="/ejercicios" class="nav-link px-2 link-dark">Ejercicios</a></li>
         <li><a href="/entrenadores" class="nav-link px-2 link-dark">Entrenadores</a></li>
         <li><a href="/sobreNosotros" class="nav-link px-2 link-dark">About Us</a></li>
